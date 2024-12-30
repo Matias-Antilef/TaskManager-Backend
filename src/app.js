feaf6508bea7b2541fe36013581363071c5d10f0
Matias Antilef <matias.romero.dv@gmail.com>
2024-12-30 17:02:29 -0300

    deploy

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, "./config/.env") });
 
 const corsOptions = {
-  origin: "https://task-manager-front-end-omega.vercel.app/",
+  origin: CLIENT_URL,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
